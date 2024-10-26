70b3aea7c352669345a6491ef086e0a7f43584c9
Owolabi <48913682+owolabidevops@users.noreply.github.com>
2024-10-26 13:00:22 +0100

    Add files via upload

---
+++ Plagiarism_Detection.py
@@ -0,0 +1,8 @@
+from difflib import SequenceMatcher
+
+with open('demofile1.txt') as one_file, open('demofile2.txt') as two_file:
+    data_file1 = one_file.read()
+    data_file2 = two_file.read()
+    
+    matches = SequenceMatcher(None, data_file1, data_file2).ratio()
+    print(f"The plagiarized content is {matches*100}% ")
